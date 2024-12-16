6f14a5e0bd11db9a6e4ddbd7b16bac871aa3dbcd
zhangdx <zdx3987@163.com>
2024-12-16 23:28:26 +0800

    增加系统托盘代码

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const {app, BrowserWindow} = require('electron')
+const {app, Tray, Menu} = require('electron')
 
-
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width:800,
-        height:600,
-        title:'CVStack'
-    })
-    win.loadFile('index.html')
-}
+let tray = null
 app.whenReady().then(() => {
-    createWindow()
+    tray = new Tray('image/secondicon.png')
+    const trayMenu = Menu.buildFromTemplate([
+        {label: '关于', type: 'normal'},
+        {label: '退出', type: 'normal', click: () => {
+                console.log('这是点击了')}}
+    ])
+    tray.setContextMenu(trayMenu)
+    tray.setToolTip('CVStack')
 })
